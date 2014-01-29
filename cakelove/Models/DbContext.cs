1e30a97132a2d4cf95434cecdbb195545c0ce46b
Shaun Luttin <shaunluttin@gmail.com>
2014-01-29 13:39:00 -0800

    Run code first migration and update the database.

---
+++ cakelove/Models/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace cakelove.Models
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+
+    }
+}
