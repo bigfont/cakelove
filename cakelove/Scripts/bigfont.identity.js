c962659d4b0c23efac2d2dd55d03400d4652616b
Shaun Luttin <shaunluttin@gmail.com>
2014-01-30 10:02:46 -0800

    Add further form examples.

--- cakelove/Scripts/bigfont.identity.js
+++ cakelove/Scripts/bigfont.identity.js
@@ -9,26 +9,43 @@ function testUserName() {
     return "testUser" + (date.getMilliseconds() * date.getSeconds());
 }
 
-myApp.controller('RegisterCtrl_Test', ['$scope', '$http', '$location', function ($scope, $http, $location) {    
+function testPassword() {
+    return null;
+}
+
+function testConfirmPassword() {
+    return null;
+}
+
+myApp.controller('RegisterCtrl_Test', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
     var userName = testUserName();
-    var url = $location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/api/account/register';    
+    var password = testPassword();
+    var confirmPassword = testConfirmPassword();
+    var url = $location.$$protocol + '://' + $location.$$host + ':' + $location.$$port + '/api/account/register';
 
-    $http({ method: 'POST', url: url, data: { UserName: userName, Password: 'password123', ConfirmPassword: 'password123' } }).
+    $scope.result = {};
+
+    $http({ method: 'POST', url: url, data: { UserName: userName, Password: password, ConfirmPassword: 'password123' } }).
         success(function (data, status, headers, config) {
 
-            $scope.result = "success";
+            $scope.result.data = data;
+            $scope.result.status = status;
+            $scope.result.headers = headers;
+            $scope.result.config = config;
 
         }).
         error(function (data, status, headers, config) {
 
-            $scope.result = "error";
-
+            $scope.result.data = data;
+            $scope.result.status = status;
+            $scope.result.headers = headers;
+            $scope.result.config = config;
         });
 
 }]);
 
-myApp.controller('RegisterCtrl_Simple', ['$scope', function ($scope, $http) {
+myApp.controller('SimpleFormCtrl', ['$scope', function ($scope) {
     $scope.master = {};
 
     $scope.update = function (user) {
@@ -42,3 +59,24 @@ myApp.controller('RegisterCtrl_Simple', ['$scope', function ($scope, $http) {
     $scope.reset();
 
 }]);
+
+myApp.controller('FormAndControlStateCtrl', ['$scope', function ($scope) {
+
+    $scope.master = {};
+
+    $scope.update = function (user) {
+        $scope.master = angular.copy(user);
+    };
+
+    $scope.reset = function () {
+        $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.isUnchanged = function (user) {
+        return angular.equals(user, $scope.master);
+    };
+
+    $scope.reset();
+
+}]);
+
