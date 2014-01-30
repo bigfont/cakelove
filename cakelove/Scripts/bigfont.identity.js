534bede9bf5d8ac0fe5a8cf6be1d56cd155c27e1
Shaun Luttin <shaunluttin@gmail.com>
2014-01-29 16:53:31 -0800

    Test username.

--- cakelove/Scripts/bigfont.identity.js
+++ cakelove/Scripts/bigfont.identity.js
@@ -2,9 +2,16 @@
 //app
 var myApp = angular.module('CakeLoveApp', []);
 
+function testUserName() {
+    var date = new Date();
+    return "testUser" + (date.getMilliseconds() * date.getSeconds());
+}
+
 myApp.controller('RegisterCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    $http({ method: 'POST', url: 'api/account/register', data: { UserName: 'Alice', Password: 'password123', ConfirmPassword: 'password123' } }).
+    var userName = testUserName();
+
+    $http({ method: 'POST', url: 'api/account/register', data: { UserName: testUserName, Password: 'password123', ConfirmPassword: 'password123' } }).
         success(function (data, status, headers, config) {
 
             $scope.result = "success";
