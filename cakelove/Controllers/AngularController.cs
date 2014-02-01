73c36497852d5513a474c234e3fe9157149e2a41
Shaun Luttin <shaunluttin@gmail.com>
2014-02-01 15:00:07 -0800

    Add a catch all controller for ng.

---
+++ cakelove/Controllers/AngularController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+using cakelove.Models;
+
+namespace cakelove.Controllers
+{
+    public class AngularController : Controller
+    {
+        protected override void HandleUnknownAction(string actionName)
+        {
+            try
+            {
+                View(actionName).ExecuteResult(ControllerContext);
+            }
+            catch (InvalidOperationException ieox)
+            {
+                ViewData["error"] = "Unknown Action:" + Server.HtmlEncode(actionName);
+                ViewData["exMessage"] = ieox.Message;
+                this.View("Error").ExecuteResult(this.ControllerContext);
+            }
+        }
+    }
+}
