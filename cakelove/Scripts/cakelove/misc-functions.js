2e594a692b343f2d9af3c20246db22ac29a18b4e
Shaun Luttin <shaunluttin@gmail.com>
2014-02-06 22:15:00 -0800

    Return module not the other thing.

--- cakelove/Scripts/cakelove/misc-functions.js
+++ cakelove/Scripts/cakelove/misc-functions.js
@@ -1,44 +1,41 @@
 ﻿var miscFunctions = (function () {
 
+    var module = {};
+
     module.InferTheHtmlInputTypeOfTheKeyValuePair = function (key, value) {
 
-        var inputType = (typeof value).toLowerCase();
+        var inputType = "text"; // this is the default input type
 
-        if (inputType === "string") {
+        if (/[a-zA-Z]/.test(key)) {
+            key = key.toLowerCase(); // set to lowercase for comparison
+        }
 
-            inputType = "text"; // this is the default input type
+        // iterate all the html input types to look for a match
+        var regex;
+        var htmlTextualInputTypes = [
+            "hidden", "text", "search", "tel", "url", "email",
+            "password", "datetime", "date", "month", "week", "time", "datetime-local", "number", "range", "color",
+            "checkbox", "radio", "file", "submit", "image"
+        ];
+        for (var i = 0; i < htmlTextualInputTypes.length; i++) {
 
-            if (/[a-zA-Z]/.test(key)) {
-                key = key.toLowerCase(); // set to lowercase for comparison
-            }
-
-            // iterate all the html input types to look for a match
-            var regex;
-            var htmlTextualInputTypes = [
-                "hidden", "text", "search", "tel", "url", "email",
-                "password", "datetime", "date", "month", "week", "time", "datetime-local", "number", "range", "color",
-                "checkbox", "radio", "file", "submit", "image"
-            ];
-            for (var i = 0; i < htmlTextualInputTypes.length; i++) {
-
-                var t = htmlTextualInputTypes[i];
-                regex = new RegExp(t);
-                if (regex.test(key)) {
-                    inputType = t;
-                    break;
-                }
+            var t = htmlTextualInputTypes[i];
+            regex = new RegExp(t);
+            if (regex.test(key)) {
+                inputType = t;
+                break;
             }
         }
 
         return inputType;
     };
 
-    module.InferTheHtmlInputAttributesOfEachKeyValuePair = function(data) {
+    module.InferTheHtmlInputAttributesOfEachKeyValuePair = function (data) {
 
         var newData = angular.copy(data);
 
         var i = 0;
-        angular.forEach(data, function(value, key) {
+        angular.forEach(data, function (value, key) {
 
             var inferredType = module.InferTheHtmlInputTypeOfTheKeyValuePair(key, value);
 
@@ -69,14 +66,14 @@
     module.AddServerSideValidationMessages = function (form, masterModel, modelState) {
 
         var properties = [];
-        angular.forEach(modelState, function(value, key) {
+        angular.forEach(modelState, function (value, key) {
 
             var property = key.replace("model.", "");
             properties.push(property);
 
         });
 
-        angular.forEach(masterModel, function(value, key) {
+        angular.forEach(masterModel, function (value, key) {
 
             var isValid = properties.indexOf(key) < 0;
             form[key].$setValidity("servervalidation", isValid);
@@ -88,6 +85,6 @@
 
     };
 
-    return miscFunctions;
+    return module;
 
 }());
