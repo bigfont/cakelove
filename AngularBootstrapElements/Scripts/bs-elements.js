ee8361be960e04234c988381aabcd2ca384e8906
Shaun Luttin <shaunluttin@gmail.com>
2014-02-11 14:35:29 -0800

    Add bs-input.

---
+++ AngularBootstrapElements/Scripts/bs-elements.js
@@ -0,0 +1,19 @@
+﻿var bsElements = angular.module('bsElements', []);
+
+bsElements.directive('bseInput', function() {
+    return {
+
+        templateUrl: "/Scripts/templates/bs-input.html",
+        scope:
+        {
+            id: '@',
+            labelText: '@',
+            placeholder: '@',
+            type:'@'
+        },
+        compile: function (el) {
+            el.removeAttr("id");
+        }
+
+    };
+});
