346e5e2a6d1ee12dcbc45646e858d4c865765dc0
Shaun Luttin <shaunluttin@gmail.com>
2014-02-11 17:13:19 -0800

    Implement file upload.

--- AngularBootstrapElements/Scripts/bs-elements.js
+++ AngularBootstrapElements/Scripts/bs-elements.js
@@ -1,18 +1,38 @@
 ﻿var bsElements = angular.module('bsElements', []);
 
+/*
+ * Handles these input types email, password
+*/
 bsElements.directive('bseInput', function() {
     return {
 
-        templateUrl: "/Scripts/templates/bs-input.html",
+        templateUrl: "/Scripts/templates/bse-input.html",
         scope:
         {
             id: '@',
-            labelText: '@',
+            lblText: '@',
             placeholder: '@',
             type:'@'
         },
         compile: function (el) {
             el.removeAttr("id");
+            el.removeAttr("type");
+        }
+
+    };
+});
+
+bsElements.directive('bseFileupload', function () {
+    return {
+
+        templateUrl: "/Scripts/templates/bse-fileupload.html",
+        scope:
+        {
+            id: '@',
+            lblText: '@'
+        },
+        compile: function (el) {
+            el.removeAttr("id");            
         }
 
     };
