efed6d81e07cce85c2c802c78f9eed08f6ecde34
Shaun Luttin <shaunluttin@gmail.com>
2014-02-15 19:07:09 -0800

    Add checkboxes to view model.

--- cakelove/Models/ClassInfoViewModel.cs
+++ cakelove/Models/ClassInfoViewModel.cs
@@ -13,66 +13,63 @@ namespace cakelove.Models
     {
         [JsonProperty("id")]
         public int Id { get; set; }
-
         [JsonProperty("className")]
         public string ClassName { get; set; }
-
         [JsonProperty("classDescription")]
         public string ClassDescription { get; set; }
-
         [JsonProperty("classType")]
         public string ClassType { get; set; }
-
         [JsonProperty("feePerStudent")]
         public int FeePerStudent { get; set; }
-
         [JsonProperty("currency")]
         public string Currency { get; set; }
-
         [JsonProperty("classSizeMin")]
         public int ClassSizeMin { get; set; }
-
         [JsonProperty("classSizeMax")]
         public int ClassSizeMax { get; set; }
-
         [JsonProperty("skillLevel")]
         public string SkillLevel { get; set; }
-
         [JsonProperty("totalTimeDayOne")]
         public int TotalTimeDayOne { get; set; }
-
         [JsonProperty("totalTimeDayTwo")]
         public int TotalTimeDayTwo { get; set; }
-
         [JsonProperty("preferredTimeDayOne")]
         public string PreferredTimeDayOne { get; set; }
-
         [JsonProperty("preferredTimeDayTwo")]
         public string PreferredTimeDayTwo { get; set; }
-
         [JsonProperty("extraTimeSetup")]
         public int ExtraTimeSetup { get; set; }
-
         [JsonProperty("extraTimeCleanup")]
         public int ExtraTimeCleanup { get; set; }
-
         [JsonProperty("suppliesWillRequireThese")]
         public string SuppliesWillRequireThese { get; set; }
-
         [JsonProperty("suppliesWillProvideThese")]
         public string SuppliesWillProvideThese { get; set; }
-
         [JsonProperty("suppliesWillSellThese")]
         public string SuppliesWillSellThese { get; set; }
-
         [JsonProperty("willNeedThisSpecialEquip")]
         public string WillNeedThisSpecialEquip { get; set; }
-
         [JsonProperty("vendorTable")]
         public string VendorTable { get; set; }
-
         [JsonProperty("hasClassImage")]
         public string HasClassImage { get; set; }
+        [JsonProperty("doSpecialSupplies")]
+        public bool? DoSpecialSupplies { get; set; }
+        [JsonProperty("doVendorTable")]
+        public bool? DoVendorTable { get; set; }
+        [JsonProperty("hasTimePreference")]
+        public bool? HasTimePreference { get; set; }
+        [JsonProperty("isMultiDay")]
+        public bool? IsMultiDay { get; set; }
+        [JsonProperty("isSelling")]
+        public bool? IsSelling { get; set; }
+        [JsonProperty("needsExtraCleanup")]
+        public bool? NeedsExtraCleanup { get; set; }
+        [JsonProperty("needsExtraSetup")]
+        public bool? NeedsExtraSetup { get; set; }
+        [JsonProperty("suppliesOption")]
+        public string SuppliesOption { get; set; } // radio
+
     }
 
 }
