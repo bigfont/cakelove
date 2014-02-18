1cc7d27e2e3b5aabd9426892913ea7d79b5f81b3
Shaun Luttin <shaunluttin@gmail.com>
2014-02-18 02:26:37 -0800

    Update sql query.

--- sql-queries/hasImage.sql
+++ sql-queries/hasImage.sql
@@ -1,10 +1,11 @@
-select * from (select u.username, imagerelativepath, c.id, hasImage
+select * from (
+select u.username, imagerelativepath, c.id, hasImage, lastmodifieddate
 from classinfo c
 join aspnetusers u on c.identityuser_id = u.id
 union
-select u.username, imagerelativepath, -1, hasImage
+select u.username, imagerelativepath, -1, hasImage, lastmodifieddate
 from biography b
 join aspnetusers u on b.identityuser_id = u.id) as t
 where t.hasImage = 1 
 or LEN(t.imagerelativepath) > 0
-order by t.id, t.username
+order by t.username, t.id
